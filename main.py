d56ed8a155943f9931ea20f428ddd3b5ac95804e
yumengtan <e0544407@u.nus.edu>
2023-03-20 20:47:51 +0800

    test

--- main.py
+++ main.py
@@ -127,12 +127,12 @@ def handle_stock_message(message):
          market_status = "aftermarket"
       else:
          market_status = "regular trading"
-      print(current_time)
       current_time = datetime.datetime.now(pytz.timezone('Asia/Singapore')).strftime("%I:%M %p") #time in SGT 12hr format
       if percent >= 0:
-        bot.send_message(message.chat.id, "The price of {} is ${:.2f} USD as at {} SGT ({}). The stock is up {:.4f}% from 24hrs.".format(stock_symbol, price, current_time, market_status, percent))
+        bot.send_message("The price of {} is ${:.2f} USD as at {} SGT ({}). The percentage change is {:.4f}% from 24hrs".format(stock_symbol, price, current_time, market_status, percent))
       else:
         percent = abs(percent)
+        print(percent)
         bot.send_message(message.chat.id, "The price of {} is ${:.2f} USD as at {} SGT ({}). The stock is down {:.4f}% from 24hrs.".format(stock_symbol, price, current_time, market_status, percent))
 
   except:
